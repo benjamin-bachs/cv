d338970328bf24665a4a6404ce5d8e2a6ea93942
benjamin-bachs <benjamin.bachs@gmail.com>
2023-03-04 09:15:21 -0300

    Redireccion luego de elegir idioma

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let español = document.getElementById("español");
+    español.addEventListener("click", function(){
+        document.getElementById("langSelection").style.display="none"
+        window.location.href = "español.html";
+    });
+    let ingles = document.getElementById("english");
+    ingles.addEventListener("click", function(){
+        document.getElementById("langSelection").style.display="none"
+        window.location.href = "english.html";
+    });
+  });
