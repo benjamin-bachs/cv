567317dbaabd7e751b452551c8861ad458a8dff0
benjamin-bachs <benjamin.bachs@gmail.com>
2023-03-06 16:06:18 -0300

    Proyecto finalizado

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var menubutton = document.getElementById("menubutton");
+    let español = document.getElementById("spanish");
+    español.addEventListener("click", function(){
+        window.location.href = "spanish.html";
+    });
+    let ingles = document.getElementById("english");
+    ingles.addEventListener("click", function(){
+        window.location.href = "english.html";
+    });
+    var menubutton = document.getElementById("language-button");
     var menu = document.getElementById("menu");
     menubutton.addEventListener("click", function() {
-        console.log("Que tu me hiciste");
         if (menu.style.display === "block") {
             menu.style.display = "none";
         } else {
